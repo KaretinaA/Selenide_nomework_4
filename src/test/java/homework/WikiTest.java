cd1e0b88859be3ed3651a30589bc52e6f681c81e
KaretinaA <aleksanna-92@mail.ru>
2023-08-03 16:51:27 +0300

    rename

--- src/test/java/homework/WikiTest.java
+++ src/test/java/homework/WikiTest.java
@@ -2,12 +2,13 @@ package homework;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class Wiki {
+public class WikiTest {
     @Test
     void selenideHomeworkTest() {
         Configuration.browserSize = "1920x1080";
@@ -18,15 +19,19 @@ public class Wiki {
         $x("//*[contains(text(), 'SoftAssertions')]").shouldHave(text("SoftAssertions"));
         $x("//*[contains(text(), 'SoftAssertions')]").click();
         Selenide.executeJavaScript("window.scroll(0, 1800);");
-        $("#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:"));
-        $("#wiki-body").shouldHave(text("@Test\n" +
-                "  void test() {\n" +
-                "    Configuration.assertionMode = SOFT;\n" +
-                "    open(\"page.html\");\n" +
-                "\n" +
-                "    $(\"#first\").should(visible).click();\n" +
-                "    $(\"#second\").should(visible).click();\n" +
-                "  }"));
+        $("#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:\n" +
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"));
+
     }
 
 }
